405c088ad81796194671e76e4dbdf6351e18bbf9
Adarsh <adarsh111789@email.com>
2026-06-05 21:43:43 +0530

    Initial calculator structure

---
+++ README.md
@@ -0,0 +1,12 @@
+# Python Calculator
+
+GitHub Collaboration Assignment
+
+Operations:
+- Addition
+- Subtraction
+- Multiplication
+- Division
+- Power
+- Modulus
+- Floor Division
---
+++ calculator.py
@@ -0,0 +1,20 @@
+def add(a, b):
+    pass
+
+def subtract(a, b):
+    pass
+
+def multiply(a, b):
+    pass
+
+def divide(a, b):
+    pass
+
+def power(a, b):
+    pass
+
+def modulus(a, b):
+    pass
+
+def floor_division(a, b):
+    pass
